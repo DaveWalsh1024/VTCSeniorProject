2d021d9127b4f234a142adce9c2593dc0d6d2c62
dwgr8golfer <davidw14@comcast.net>
2018-03-16 11:38:10 -0400

    Code updates

---
+++ app/src/main/java/com/example/david/scorecardpro/CreateTeamActivity.java
@@ -0,0 +1,91 @@
+package com.example.david.scorecardpro;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by jacklavallee on 2/27/18.
+ */
+
+public class CreateTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_team);
+
+        Button addPlayer = (Button) findViewById(R.id.addPlayerButton);
+        addPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(CreateTeamActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.add_player, null);
+                final Button addPlayer = (Button) findViewById(R.id.addPlayerButton);
+                final EditText firstName = (EditText) findViewById(R.id.firstNameEditText);
+                final EditText lastName = (EditText) findViewById(R.id.lastNameEditText);
+                final EditText number = (EditText) findViewById(R.id.numberEditText);
+                final Button submit = (Button) findViewById(R.id.addPlayerSubmit);
+
+                final TableLayout tableLayout = (TableLayout) findViewById(R.id.playerList_Table);
+                final TableRow tableRow = new TableRow(CreateTeamActivity.this);
+                final TextView firstNameRow = new TextView(CreateTeamActivity.this);
+                final TextView lastNameRow = new TextView(CreateTeamActivity.this);
+                final TextView numberRow = new TextView(CreateTeamActivity.this);
+
+                tableRow.layout(5,5,5,5);
+
+                firstNameRow.setTextSize(32);
+                lastNameRow.setTextSize(32);
+                numberRow.setTextSize(32);
+
+
+                addPlayer.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!firstName.getText().toString().isEmpty() && !lastName.getText().toString().isEmpty() && !number.getText().toString().isEmpty()) {
+                            Toast.makeText(CreateTeamActivity.this,
+                                    "Player Added!",
+                                    Toast.LENGTH_SHORT).show();
+
+                            submit.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                    tableLayout.addView(tableRow, new TableLayout.LayoutParams(
+                                            TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT
+                                    ));
+
+                                    firstNameRow.setText(firstName.getText().toString());
+                                    tableRow.addView(firstNameRow);
+
+                                    lastNameRow.setText(lastName.getText().toString());
+                                    tableRow.addView(lastNameRow);
+
+                                    numberRow.setText(number.getText().toString());
+                                    tableRow.addView(numberRow);
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(CreateTeamActivity.this,
+                                    "Player Addition Unsuccessful",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/david/scorecardpro/RunnerView.java
@@ -0,0 +1,135 @@
+package com.example.david.scorecardpro;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by jacklavallee on 2/21/18.
+ */
+
+public class RunnerView extends AppCompatTextView implements View.OnTouchListener {
+
+    public RunnerView(ScoringActivity context, Player player) {
+        super(context);
+        scoringActivity = context;
+        this.player = player;
+        this.base = null;
+        setOnTouchListener(this);
+        setWidth(width);
+        setHeight(height);
+        setMaxWidth(width);
+        setMaxHeight(height);
+        setGravity(Gravity.CENTER);
+        setText(String.valueOf(player.getNumber()));
+        setTextColor(Color.rgb(255,255,255));
+        setTextSize(26);
+        setBackground(getResources().getDrawable(R.drawable.onbase_img));
+    }
+
+    public RunnerView(Player player) {
+        this(currentActivity, player);
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent e) {
+        int newX;
+        int newY;
+
+        switch (e.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = (int) e.getX();
+                lastY = (int) e.getY();
+                viewLastLeft = view.getLeft();
+                viewLastTop = view.getTop();
+                viewLastRight = view.getRight();
+                viewLastBottom = view.getBottom();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                newX = (int) e.getX();
+                newY = (int) e.getY();
+                scoringActivity.runnerMove(this,newX - lastX, newY - lastY);
+                break;
+            case MotionEvent.ACTION_UP:
+                scoringActivity.snapToBase(this);
+                break;
+        }
+        return true;
+    }
+
+    public int getViewLastLeft() {
+        return viewLastLeft;
+    }
+
+    public int getViewLastTop() {
+        return viewLastTop;
+    }
+
+    public int getViewLastRight() {
+        return viewLastRight;
+    }
+
+    public int getViewLastBottom() {
+        return viewLastBottom;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player newPlayer) {
+        player = newPlayer;
+    }
+
+    public static void setScoringActivity(ScoringActivity sa) {
+        currentActivity = sa;
+    }
+
+    public Base getBase() {
+        return base;
+    }
+
+    public void setBase(Base newBase) {
+        if (newBase == null) {
+            currentActivity.removeFromBase(this);
+        }
+        else if (base == null) {
+            currentActivity.addToBase(this, newBase);
+            this.base = newBase;
+
+        }
+        else if (base != newBase) {
+            currentActivity.moveToBase(this, base, newBase);
+            this.base = newBase;
+        }
+    }
+
+    public void removePlayer() {
+        this.player = null;
+    }
+
+
+
+    public int lastX;
+    public int lastY;
+    public int viewLastLeft;
+    public int viewLastTop;
+    public int viewLastRight;
+    public int viewLastBottom;
+    private ScoringActivity scoringActivity;
+    private static ScoringActivity currentActivity;
+    private Player player;
+    private Base base;
+    public static final int width = 100;
+    public static final int height = 100;
+
+
+}
